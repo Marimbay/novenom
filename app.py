b0144ea6445f9923778669ccd5caea556c6f7c7b
GyeonghwanNoh <korea07291@gmail.com>
2025-05-19 15:50:00 +1000

    Novenom Updates

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os, sys
+print(">>> TEMPLATE_FOLDER:", os.path.join(os.path.dirname(__file__), 'templates'), file=sys.stderr)
+from flask import Flask, render_template, request, url_for
 import os
-from werkzeug.utils import secure_filename # <-- This is for the buttons
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads'
+
+# Configure upload folder
+UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Allow only certain file extensions
+# Allowed file extensions
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return (
+        '.' in filename and
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    )
 
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    error = None
     image_url = None
-    if request.method == "POST":
-        if 'file' not in request.files:
-            return "No file part"
-        file = request.files['file']
-        if file.filename == '':
-            return "No selected file"
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            image_url = url_for('static', filename=f'uploads/{filename}')
-    return render_template("index.html", image_url=image_url)
 
-if __name__ == "__main__":
-    app.run(host="10.89.48.37", port=5000) # <-- Change "10.89.48.37" IP to your Raspberry Pi IP
+    if request.method == 'POST':
+        # Check file part
+        if 'file' not in request.files:
+            error = 'No file part'
+        else:
+            file = request.files['file']
+            if file.filename == '':
+                error = 'No selected file'
+            elif allowed_file(file.filename):
+                os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                image_url = url_for('static', filename=f'uploads/{filename}')
+            else:
+                error = 'File type not allowed'
+
+    return render_template('upload.html', error=error, image_url=image_url)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
